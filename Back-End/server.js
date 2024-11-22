1db13be9f334ba104ea4b8f2424665870efd38a4
aminsheikh00 <amin.sheikhazimi221@gmail.com>
2024-11-22 17:07:54 +0330

    update server,js for ejs

--- Back-End/server.js
+++ Back-End/server.js
@@ -13,9 +13,9 @@ const expressLayouts = require('express-ejs-layouts')
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
-// app.set('view engine', 'ejs');
-// app.use(expressLayouts);
-// app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.use(expressLayouts);
+app.use(express.static('public'));
 
 // routing
 app.use('/api', menuRouter)
