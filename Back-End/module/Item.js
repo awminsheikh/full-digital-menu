b9812eae8a8dca8da578c4e1ea27f8dbdaec9be1
aminsheikh00 <amin.sheikhazimi221@gmail.com>
2024-11-22 17:04:19 +0330

    Module

---
+++ Back-End/module/Item.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const itemSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    category: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Category',
+        required: true
+    }
+});
+
+const Item = mongoose.model('Item', itemSchema);
+module.exports = Item;
