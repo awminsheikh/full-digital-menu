7bf2761e20efbc0be6eeac1eb4dd3cdc5f61b42c
aminsheikh00 <amin.sheikhazimi221@gmail.com>
2024-11-22 17:07:22 +0330

    remove useless index Router

--- Back-End/server.js
+++ Back-End/server.js
@@ -1,36 +1,37 @@
-const express = require('express')
+const express = require("express")
 const app = express()
 
-// Set view engine and views directory  
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
-app.set('layout', 'layouts/layout'); // Correctly specifies the layout
 
-// Middleware setup  
-app.use(expressLayouts);
+const path = require('path')
 
-// connect to mongoDb
-const mongoose = require('mongoose')
-mongoose.connect(process.env.DATABASE_URL, { // Use DATABASE_URL  
-    useNewUrlParser: true,  // Optional: Manage deprecation warnings  
-    useUnifiedTopology: true // Optional: Use the new Server Discover and Monitoring engine  
-});
-const db = mongoose.connection
-db.on('error', (error) => {
-    console.error(error)
-})
-db.once('open', () => {
-    console.log('conected to Mongoose')
-})
-
-// routing
-const indexRouter = require('./routes/index')
 const menuRouter = require('./routes/menu')
 
-app.use('/', indexRouter)
+// Middleware setup  
+const cors = require('cors')
+const expressLayouts = require('express-ejs-layouts')
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+// app.set('view engine', 'ejs');
+// app.use(expressLayouts);
+// app.use(express.static('public'));
+
+// routing
 app.use('/api', menuRouter)
 
-// Start server  
+// connect to MongoDb
+const dotenv = require('dotenv')
+dotenv.config()
+const mongoose = require('mongoose')
+const { error } = require("console")
+
+mongoose.connect(process.env.DATABASE_URL);
+const db = mongoose.connection
+db.on('error', () => console.error(error))
+db.once('open', () => console.log("Conected to data base"))
+
+//listen 
 app.listen(process.env.PORT || 3000, () => {
     console.log(`Server running on port ${process.env.PORT || 3000}`);
 });
